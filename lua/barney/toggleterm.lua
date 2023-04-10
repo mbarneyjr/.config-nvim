c913db1170d9d4f43fa3b1b053938248587843bd
Michael Barney, Jr <mbarneyme@gmail.com>
2023-04-09 21:49:00 -0400

    remove escape keymap for terminal

--- lua/barney/toggleterm.lua
+++ lua/barney/toggleterm.lua
@@ -8,7 +8,7 @@ require('toggleterm').setup({
   end,
 })
 
-vim.keymap.set('t', '<esc>', '<c-\\><c-n>')
+vim.keymap.set('t', '<c-t>', '<c-\\><c-n>')
 vim.keymap.set('n', '<c-t>', function() return '<cmd>ToggleTerm direction=horizontal size=' .. vim.v.count .. '<cr>' end, { expr = true })
 vim.keymap.set('n', '<c-v>', function() return '<cmd>ToggleTerm direction=vertical size=' .. vim.v.count .. '<cr>' end, { expr = true })
 
