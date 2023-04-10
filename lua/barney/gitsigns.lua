86c9c47b991a439255da228f6c4ab34c0152eb48
Michael Barney, Jr <mbarneyme@gmail.com>
2023-04-10 09:59:44 -0400

    move git-blame to eol

--- lua/barney/gitsigns.lua
+++ lua/barney/gitsigns.lua
@@ -9,7 +9,7 @@ require('gitsigns').setup {
   current_line_blame = true,
   current_line_blame_opts = {
     virt_text = true,
-    virt_text_pos = 'right_align',
+    virt_text_pos = 'eol',
     delay = 0,
     ignore_whitespace = false,
   },
