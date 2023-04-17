f89523162df42ce5fe3a1bf18326212dd7ebc04e
Michael Barney, Jr <mbarneyme@gmail.com>
2023-04-17 10:04:24 -0400

    remap vertical terminal to <c-s>

--- lua/barney/toggleterm.lua
+++ lua/barney/toggleterm.lua
@@ -9,7 +9,6 @@ require('toggleterm').setup({
 })
 
 vim.keymap.set('t', '<c-t>', '<c-\\><c-n>')
+vim.keymap.set('t', '<c-s>', '<c-\\><c-n>')
 vim.keymap.set('n', '<c-t>', function() return '<cmd>ToggleTerm direction=horizontal size=' .. vim.v.count .. '<cr>' end, { expr = true })
-vim.keymap.set('n', '<c-v>', function() return '<cmd>ToggleTerm direction=vertical size=' .. vim.v.count .. '<cr>' end, { expr = true })
-
-
+vim.keymap.set('n', '<c-s>', function() return '<cmd>ToggleTerm direction=vertical size=' .. vim.v.count .. '<cr>' end, { expr = true })
