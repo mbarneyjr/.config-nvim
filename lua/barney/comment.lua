fd5bf1d4fec9da8ab3d6d558f8edfb1ea9f2d340
Michael Barney, Jr <mbarneyme@gmail.com>
2023-08-28 16:48:26 -0400

    add comment option for graphviz/dot

--- lua/barney/comment.lua
+++ lua/barney/comment.lua
@@ -1,5 +1,8 @@
 require('nvim_comment').setup({
   hook = function()
+    if vim.api.nvim_buf_get_option(0, 'filetype') == 'dot' then
+      vim.api.nvim_buf_set_option(0, 'commentstring', '// %s')
+    end
     if vim.api.nvim_buf_get_option(0, 'filetype') == 'terraform' then
       vim.api.nvim_buf_set_option(0, 'commentstring', '# %s')
     end
