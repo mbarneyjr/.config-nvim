055c7474e49525e8125a3545d89ae47607e1ab17
Michael Barney, Jr <mbarneyme@gmail.com>
2023-12-12 13:17:44 -0500

    ignore git from telescope search

--- lua/barney/plugins/telescope.lua
+++ lua/barney/plugins/telescope.lua
@@ -16,7 +16,7 @@ return {
       defaults = {
         file_ignore_patterns = {
           "node_modules",
-          ".git",
+          ".git/",
           "cdk.out",
         },
         mappings = {
