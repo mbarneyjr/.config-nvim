4e250af52eac5b3fb9a1b8974bffe588e0b2cfd9
Michael Barney, Jr <mbarneyme@gmail.com>
2023-12-12 13:15:08 -0500

    update ts_context_commentstring plugin

--- lua/barney/plugins/comment.lua
+++ lua/barney/plugins/comment.lua
@@ -5,15 +5,14 @@ return {
     "JoosepAlviste/nvim-ts-context-commentstring",
   },
   config = function()
+    vim.g.skip_ts_context_commentstring_module = true
     -- import comment plugin safely
     local comment = require("Comment")
 
-    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+    local ts_context_commentstring = require("ts_context_commentstring")
 
-    -- enable comment
-    comment.setup({
-      -- for commenting tsx and jsx files
-      pre_hook = ts_context_commentstring.create_pre_hook(),
-    })
+    ts_context_commentstring.setup({})
+
+    comment.setup({})
   end,
 }
